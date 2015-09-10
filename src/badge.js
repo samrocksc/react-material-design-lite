3c5d0bf4d28bb1b9976b6689ad6d0085837924e6
haoxin <coderhaoxin@outlook.com>
2015-09-11 01:24:19 +0800

    Add Badge

---
+++ src/badge.js
@@ -0,0 +1,53 @@
+'use strict';
+
+require('material-design-lite/material.min.css');
+
+const React = require('react');
+const mdl = require('material-design-lite/material.min');
+const classnames = require('classnames');
+
+const baseClasses = {
+  'mdl-badge': true
+};
+
+class Badge extends React.Component {
+
+  componentDidMount(){
+    const node = React.findDOMNode(this);
+    mdl.upgradeElement(node, 'MaterialBadge');
+  }
+
+  componentWillUnmount(){
+    const node = React.findDOMNode(this);
+    mdl.downgradeElements(node);
+  }
+
+  render(){
+    const {
+      children,
+      className,
+      background = true,
+      overlap
+    } = this.props;
+
+    const classes = classnames(baseClasses, {
+      'mdl-badge--no-background': !background,
+      'mdl-badge--overlap': overlap
+    }, className);
+
+    return (
+      <a {...this.props} className={classes}>
+        {children}
+      </a>
+    )
+  }
+}
+
+Badge.protTypes = {
+  href: React.PropTypes.string,
+  className: React.PropTypes.string,
+  background: React.PropTypes.bool,
+  overlap: React.PropTypes.bool
+};
+
+module.exports = Badge;
