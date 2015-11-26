783c02eef4c7771e9139e3b2395f13f125a6f144
Sam Clark <samrocksc@gmail.com>
2015-11-25 19:27:25 -0700

    updated index to work properly
    shows a lot better now.

--- example/index.js
+++ example/index.js
@@ -1,11 +1,58 @@
 'use strict';
-
+//TODO add checked function
+//TODO Make grouping easier
 const React = require('react');
 const ReactDOM = require('react-dom');
+const { Tooltip, Textfield, Switch, Spinner, Slider, Radio, Progress, IconToggle, Button, Badge, Checkbox } = require('../');
+
+const exampleStyle = {
+  margin: 'auto',
+  width: '800',
+  border: '1px solid black',
+  padding: '10px'
+}
 
 const exampleTree = (
-  <div>
-    Examples
+  <div style={exampleStyle}> 
+    <h1>Examples</h1>
+
+    <h4>Badges</h4>
+      <Badge data-badge="2">Badge</Badge>
+
+    <h4>Buttons</h4>
+      <Button>Test Button</Button>
+
+    <h4>Checkboxes</h4>
+      <Checkbox />
+
+    <h4>Icon Toggle</h4>
+      <IconToggle ripple id='2' label='test' />
+      <br/>
+
+    <h4>Progress</h4>
+      <Progress indeterminate/>
+
+    <h4>Radio Buttons</h4>
+      <Radio id='radioTest1' name='radioTest' label='Radio Button 1' value='1' ripple>Test</Radio>
+
+    <h4>Sliders</h4>
+    <Slider/>
+
+    <h4>Spinners</h4>
+      <Spinner active/>
+
+    <h4>Switches</h4>
+      <Switch id='test' label='Switch' ripple/>
+
+    <h4>Input Fields</h4>
+      <Textfield type='text' />
+
+    <h4>Tooltips</h4>
+      <span id='tt2'>Hover Me</span>
+      <Tooltip id='tt2'>Tooltip</Tooltip>
+      
+    <h4>More to come....</h4>
+
   </div>
 );
 
